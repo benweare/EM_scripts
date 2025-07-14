7571dad29d93289d1237751768fab4ec77350367
Benjamin Weare <92488183+benweare@users.noreply.github.com>
2025-07-14 09:34:47 +0100

    added scripts

---
+++ calculations/Ebeam_wavelength_nonrelativisitic.m
@@ -0,0 +1,64 @@
+%% Calculating Wavelength for Accelerating Voltage
+% (the maximum transferrable energy from an electron beam of energy X keV to an element)
+% Wavelength formula from Williams and Carter (p13 2nd ed.)
+%% For Loop Control
+x=1;
+R = zeros(length(x),1);
+X = zeros(length(x),1);
+T= zeros(length(x),1);
+for x =1:1000
+%% Defining parameters
+kev = x*1000; % This is the accelerating voltage for the equation in keV
+
+%% Preliminary calculations
+% Resting mass of electron in kg
+m0 = 9.10938356*10^-31;
+% Speed of light in m/s
+c = 299792458;
+% Planck's constant in m2kg/s
+h = 6.62607004*10^-34;
+% Energy of incident electrons in J
+e = kev*(1.60217662*10^-19);
+%Velocity of electrons
+v=((2*e)/m0)^(0.5);
+%% Calculating Wavelength
+L=h/(m0*v); % in metres
+LA=L*10^(10); %in Angstroms
+Ln=L*10^(9); %in nanometres
+Lp=L*10^(12); %in picometres
+
+T(x)=L; %wavelength in metres
+R(x)=Ln;%wavelength in Angstroms
+Q(x)=LA;%wavelength in nanometres
+S(x)=Lp;%wavelength in nanometres
+X(x)=x; % :p
+x=x+1;
+end
+R=R.';
+X=X.';
+T=T.';
+Q=Q.';
+S=S.';
+empty_1=zeros([1000 1]);
+empty_2=zeros([1000 1]);
+Results=table(X,T,Q,R,S,empty_1,empty_2);
+Results.Properties.VariableNames{1} = 'Accelerating_Voltage_keV';
+Results.Properties.VariableNames{2} = 'Wavelength_m';
+Results.Properties.VariableNames{3} = 'Wavelength_nm';
+Results.Properties.VariableNames{4} = 'Wavelength_A';
+Results.Properties.VariableNames{5} = 'Wavelength_pm';
+Results.Properties.VariableNames{6} = 'log_voltage';
+Results.Properties.VariableNames{7} = 'log_wavelength_m';
+Results.log_voltage=log10(Results.Accelerating_Voltage_keV);
+Results.log_wavelength_m=log10(Results.Wavelength_m);
+e_beam_wavelength=Results; %enter table name here
+clear R X a amass c e h kev t T m0  tmax x Results L LA Ln Q v Lp S empty_1 empty_2
+%% Plot Results
+plot(e_beam_wavelength.Accelerating_Voltage_keV, e_beam_wavelength.Wavelength_nm,'Color', 1/255*[255 0 0],'LineWidth',2);
+xlabel('Accelerating Voltage / keV','FontSize',36,'FontName','Calibri');
+ylabel('Wavelength (non-relativitic) / nm','FontSize',36,'FontName','Calibri');
+set(gca,'FontName','Calibri')
+set(gca, 'linewidth', 2);
+set(gca, 'FontSize', 28);
+set(gca,'TickDir','out');
+set(0,'defaultfigurecolor',[1 1 1]);
