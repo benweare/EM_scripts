b4a6fadc8f61ceae2345b972959207b8bc1e01d0
Benjamin Weare <92488183+benweare@users.noreply.github.com>
2026-05-07 16:33:36 +0100

    Create pyJEM_monitor_vacuum.py
    Created script to make live plot of column vacuum via PyJEM.

---
+++ PyJEM/pyJEM_monitor_vacuum.py
@@ -0,0 +1,213 @@
+'''
+Script to monitor the Penning gauge vacuum via PyJEM.
+
+Bbased on DM Scripting Site script Emission Monitor.
+BLW @ nmRC, 07-05-2026
+Work in progress.
+'''
+
+import matplotlib
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+
+import tkinter as tk
+import threading
+import time
+from datetime import datetime
+
+from PyJEM import TEM3
+
+TEM3.connect()
+
+
+# This class creates and manages the main dialog
+class Main_Dialog:
+    # Python constructor - use to create the main dialog 
+    def __init__(self, master):
+        self.master = master
+        self.frame = tk.Frame(self.master)
+        self.master.title("Vacuum Monitor")
+        self.master.configure(bg='#FFFFFF')
+      
+        # create a button which will invoke a sub-dialog to provide a value
+        self.start_button = tk.Button(self.master, text = 'Start', padx=6, pady=3, command = self.start_monitoring_response)
+        self.stop_button = tk.Button(self.master, text = 'Stop', padx=6, pady=3, command = self.end_monitoring_response)
+        
+        # Create a list of intervals with which to select the sampling time
+        self.interval_label=tk.Label(self.master, text="Interval/s")
+
+        self.interval_list=['1','2','5', '10', '20', '30', '60', '120', '240']
+        
+        self.interval_val=tk.IntVar()
+        self.interval_default_val=1
+        self.interval_val.set(self.interval_default_val)
+
+        self.interval_selector=tk.OptionMenu(self.master, self.interval_val, *self.interval_list)
+        self.interval_selector.config(width=1)
+        self.interval_selector["state"]='normal'# state can be normal or disabled
+    
+        
+        # Create a field which displays the emission value. 
+        self.mylabel=tk.Label(self.master, text= "Pressure: ")
+        self.EmissionVal = tk.IntVar()
+        self.EmissionVal.set(0) # default value
+        self.mainentry = tk.Entry(self.master,textvariable=self.EmissionVal, width=7, state="normal")
+        
+        # Create a Matplotlib grpah.
+        self.fig = matplotlib.figure.Figure(figsize = (3, 3), layout='tight', dpi = 100)
+        self.plot1 = self.fig.add_subplot(111)
+        self.plot1.set_ylabel( "Pressure" )
+        self.plot1.set_xlabel( "Live time / s" )
+        self.plot1.set_ylim([50, 100])
+        self.plot1.set_xlim([0, 30])
+        self.x_data = [0]
+        self.y_data = [0]
+        self.plot1.plot( self.x_data, self.y_data )
+        
+        # Assemble the items into the dialog
+        self.mylabel.grid(column=0, row=0)
+        self.mainentry.grid(column=1, row=0)
+        
+        self.interval_label.grid(column=0, row=1)
+        self.interval_selector.grid(column=1, row=1, pady=5)
+
+        self.start_button.grid(column=0, row=2, pady=5)
+        self.stop_button.grid(column=1, row=2)
+        
+        self.master.config(border=10)
+        
+        self.canvas = FigureCanvasTkAgg(self.fig,master = self.master)
+        self.canvas.draw()
+        self.canvas.get_tk_widget().grid( column=0, row=3 )
+        
+        
+        # A flag for ending the thread
+        self.terminate_thread_flag=0
+        
+        
+        # create an instance of the VACUUM3 module.
+        self.vac_module=TEM3.VACUUM3()
+        return
+
+    def _update_graph( self ):
+        '''
+        Update the graph by drawing it with the new data.
+        '''
+        self.plot1.clear()
+        self.plot1.plot( self.x_data, self.y_data )
+        self.plot1.set_ylim([50, 100])
+        if ( self.x_data[0] == 0 ):
+            self.plot1.set_xlim([0, 30])
+        else:
+            self.plot1.set_xlim([self.x_data[0], self.x_data[-1]])
+        self.plot1.set_ylabel( "Pressure" )
+        self.plot1.set_xlabel( "Live time / s" )
+        self.canvas.draw()
+        return
+    
+    # Python Destructor - called when the main dialog is closed. Could be used for stopping a thread
+    # closing a file, restoring default settings etc. Destructors are optional    
+    def __del__(self):
+        #print("Destructor called when Main Dialog is closed.")
+        self.terminate_thread_flag=1
+ 
+    def _update_vals( self, interval, vac ):
+        '''
+        Update the data used to draw the graph.
+        '''
+        self.y_data.append( vac )
+        self.x_data.append( self.x_data[-1]+interval )
+        if (len(self.x_data) > 30):
+            self.x_data.pop(0)
+            self.y_data.pop(0)
+        return
+    
+    
+    # This furntion is run as a thread
+    def monitor_emission(self):
+        print("\nMonitoring emission:\n")
+        do_break=0
+        
+        # Source the interval from the dialog
+        
+        interval=self.interval_val.get()
+        
+        
+        # infinite sampling loop Broken out of by pressing Stop
+        
+        while 1:
+            # Source the emission
+        
+            my_emission=self.vac_module.GetPegInfo( 0 )[0]
+        
+        
+            # Get the current time
+            
+            timestring=datetime.now()
+            nowtime=timestring.strftime("%H:%M:%S")
+        
+        
+            # Output the emission and time to the Console
+        
+            print("\rPenning 0 = "+str("%.2f" % my_emission)+" Time = "+nowtime, end="")
+            
+            # Update the dialog
+            self.EmissionVal.set(str("%.2f" % my_emission))
+            self.mainentry.update_idletasks()
+            
+            # Break the sampling interval into 1s interval and check for
+            # a stop press every second
+            
+            for i in range(interval):
+                if self.terminate_thread_flag==1:
+                    do_break=1
+                    break
+                time.sleep(1)
+            
+            self._update_vals( interval, my_emission )
+            self._update_graph()
+            
+            #print( self.x_data, self.y_data )
+            #self.canvas.get_tk_widget().grid( column=0, row=3 )
+            
+                
+            if do_break==1:
+                break
+        
+        # Reset the dialog
+         
+        print("\nThread ended")
+        self.terminate_thread_flag=0
+        self.start_button.config(state="normal")
+        return
+
+      # responds when the Start button is pressed
+    def start_monitoring_response(self):
+        self.start_button.config(state="disabled")
+        self.terminate_thread_flag=0
+        print("\nStart pressed")
+
+        self.thread_id=threading.get_ident()
+        print("Thread id = "+str(self.thread_id))
+        threading.Timer(0, self.monitor_emission).start()
+
+
+
+     # responds when the Press Me button is pressed
+ 
+    def end_monitoring_response(self):
+        print("Stop pressed")
+        self.terminate_thread_flag=1
+        self.start_button.config(state="normal")
+
+ # End of Main_Dialog class
+
+
+
+# Call the main function. Use the if __name__ etc to ensure this script will not
+# be run immdediately if loaded as a library
+
+if __name__=='__main__':
+    root = tk.Tk()
+    app = Main_Dialog(root)
+    root.mainloop()
